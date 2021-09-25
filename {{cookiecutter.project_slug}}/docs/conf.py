913de1f013e885e0c75779dbac82700cf19de399
Philip King <king.phil@gmail.com>
2021-09-25 16:48:23 -0500

    MVP w/whitespace problems: isort

--- {{cookiecutter.project_slug}}/docs/conf.py
+++ {{cookiecutter.project_slug}}/docs/conf.py
@@ -11,7 +11,7 @@
 #
 # All configuration values have a default; values that are commented out
 # serve to show the default.
-
+#
 # If extensions (or modules to document with autodoc) are in another
 # directory, add these directories to sys.path here. If the directory is
 # relative to the documentation root, use os.path.abspath to make it
@@ -19,9 +19,10 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
 
 import {{ cookiecutter.project_slug }}
+
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- General configuration ---------------------------------------------
 
